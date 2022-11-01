ae9ae8c0027ff0491cdd2eec1874cf2c4fd711fa
Ramesh Vyas <rameshovyas@gmail.com>
2022-11-01 18:33:34 +0530

    First Commit
    Added files for the first time in this repo

---
+++ Task.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binding
+{
+    public class Task : INotifyPropertyChanged
+    {
+        private string _description;
+        private string _name;       
+        private TaskType _type;
+        private int _priority;
+
+        public Task() { }
+
+        public Task(string name,string description,int priority,TaskType type)
+        {
+            _name = name;
+            _description = description;
+            _priority = priority;
+            _type = type;
+        }
+
+        public string TaskName
+        {
+            get { return _name; }
+            set
+            {
+                _name  = value;
+                OnPropertyChanged("TaskName");
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set 
+            {
+                _description = value;
+                OnPropertyChanged("Desscription");
+            }
+        }
+
+        public int Priority
+        {
+            get { return _priority; }
+            set 
+            {
+                _priority = value;
+                OnPropertyChanged("Priority");
+            }
+        }
+
+        public TaskType Type
+        {
+            get {  return _type; }
+            set
+            {
+                _type = value;
+                OnPropertyChanged("Type");
+            }
+        }
+
+        public override string ToString() => _name;       
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string info)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+    }
+}
---
+++ Tasks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binding
+{
+    public class Tasks : ObservableCollection<Task>
+    {
+        public Tasks()
+        {
+            Add(new Task("Groceries", "Pick up Groceries from store", 2, TaskType.Home));
+            Add(new Task("Laundry", "Do Laundry for my family", 2, TaskType.Home));
+            Add(new Task("Code Review", "Review code for my new project", 1, TaskType.Work));
+            Add(new Task("Paint", "Paint my office walls", 3, TaskType.Work));
+            Add(new Task("Dinner", "Prepare diner for family", 1, TaskType.Home));
+            Add(new Task("Walk", "Take walk after dinner", 2, TaskType.Home));
+        }
+    }
+}
